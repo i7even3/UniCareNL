16bf4a322eb4076ebc2461c9fe8e3993c8342e96
i7even3 <Rory.db07@gmail.com>
2025-05-21 09:32:31 +0200

    Create privacy.tsx
    add privacy.tsx

---
+++ pages/privacy.tsx
@@ -0,0 +1,12 @@
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <h1>Privacybeleid</h1>
+      <p>
+        Bij UniCareNL nemen we privacy serieus. Je gegevens worden niet gedeeld met derden en
+        alleen gebruikt voor interne doeleinden.
+      </p>
+    </div>
+  );
+}
+
