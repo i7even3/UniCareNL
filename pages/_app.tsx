6fe6513ad0f391977b38f97cedc1ba19fd6c4931
i7even3 <Rory.db07@gmail.com>
2025-06-03 22:47:22 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from 'react';
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Sidebar from '@/components/Sidebar';
+import Footer from '@/components/Footer';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </>
+    <div className="flex">
+      <Sidebar />
+      <main className="ml-64 flex-1 min-h-screen bg-gray-50 p-6">
+        <Component {...pageProps} />
+        {isClient && <Footer />}
+      </main>
+    </div>
   );
 }
