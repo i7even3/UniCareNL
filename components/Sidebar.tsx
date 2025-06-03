d6700798eb1839486088ee2e3652a86465de5878
i7even3 <Rory.db07@gmail.com>
2025-06-03 22:43:54 +0200

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,38 +1,30 @@
 import Link from 'next/link';
-import { Dispatch, SetStateAction } from 'react';
 
-interface SidebarProps {
-  isOpen: boolean;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
-}
-
-export default function Sidebar({ isOpen, setIsOpen }: SidebarProps) {
+export default function Navbar() {
   return (
-    <div
-      className={`fixed inset-y-0 left-0 bg-white shadow-md transform ${
-        isOpen ? 'translate-x-0' : '-translate-x-full'
-      } transition-transform duration-200 ease-in-out z-50 md:static md:translate-x-0 md:shadow-none`}
-    >
-      <nav className="flex flex-col p-4 space-y-2">
-        <Link href="/">
-          <a className="text-blue-800 hover:underline" onClick={() => setIsOpen(false)}>Home</a>
-        </Link>
-        <Link href="/about">
-          <a className="text-blue-800 hover:underline" onClick={() => setIsOpen(false)}>Over</a>
-        </Link>
-        <Link href="/problemen">
-          <a className="text-blue-800 hover:underline" onClick={() => setIsOpen(false)}>Problemen</a>
-        </Link>
-        <Link href="/demo">
-          <a className="text-blue-800 hover:underline" onClick={() => setIsOpen(false)}>Demo</a>
-        </Link>
-        <Link href="/contact">
-          <a className="text-blue-800 hover:underline" onClick={() => setIsOpen(false)}>Contact</a>
-        </Link>
-        <Link href="/privacy">
-          <a className="text-blue-800 hover:underline" onClick={() => setIsOpen(false)}>Privacy</a>
-        </Link>
-      </nav>
-    </div>
+    <nav className="bg-gray-100 p-4 shadow">
+      <ul className="flex space-x-6">
+        <li>
+          <Link href="/" className="text-gray-800 hover:text-blue-600">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" className="text-gray-800 hover:text-blue-600">
+            Over ons
+          </Link>
+        </li>
+        <li>
+          <Link href="/demo" className="text-gray-800 hover:text-blue-600">
+            Demo
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className="text-gray-800 hover:text-blue-600">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
